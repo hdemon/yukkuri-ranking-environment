805c8d4d321ff3017dff446ee033bb79845a1358
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-20 05:41:34 +0900

    modify a cookbook to setup ruby

--- chef/site-cookbooks/build-ruby/recipes/default.rb
+++ chef/site-cookbooks/build-ruby/recipes/default.rb
@@ -10,4 +10,34 @@
 include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 
-rbenv_ruby "2.1.0-preview2"
+rbenv_ruby "2.1.0-preview2" do
+  not_if { File.exists?("/home/yukkuri/rbenv/versions/2.1.0-preview2") }
+  ruby_version "2.1.0-preview2"
+  global true
+  force true
+end
+
+package "gem" do
+  action :install
+end
+
+execute "rbenv.global" do
+  cwd "/home/yukkuri"
+  user "yukkuri"
+  command "ls > a"
+end
+
+execute "rbenv.global" do
+  not_if { File.exists?("/home/yukkuri/rbenv/versions/2.1.0-preview2") }
+  cwd "/home/yukkuri"
+  user "yukkuri"
+  command "/home/yukkuri/rbenv/bin/rbenv global '2.1.0-preview2'"
+end
+
+rbenv_gem "bundler" do
+  ruby_version "2.1.0-preview2"
+end
+
+# gem_package "bundler" do
+#   action :install
+# end
