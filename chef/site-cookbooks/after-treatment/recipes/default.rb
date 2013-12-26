ec8dd0025563c4b5ad20236c73bd3e9cda487bfa
Masami Yonehara <zeitdiebe@gmail.com>
2013-12-26 23:11:08 +0900

    fix a bug in building ruby

--- chef/site-cookbooks/after-treatment/recipes/default.rb
+++ chef/site-cookbooks/after-treatment/recipes/default.rb
@@ -7,19 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# execute "rbenv.global" do
-#   cwd "/home/yukkuri"
-#   user "yukkuri"
-#   # command "/home/yukkuri/rbenv/bin/rbenv rehash && /home/yukkuri/rbenv/bin/rbenv versions > a"
-#   command "/home/yukkuri/rbenv/bin/rbenv global #{node[:rbenv][:version]}"
-# end
-
-include_recipe "rbenv::default"
-include_recipe "rbenv::ruby_build"
-
-rbenv_ruby node[:rbenv][:version] do
-  # not_if { File.exists?("/home/yukkuri/rbenv/versions/#{node[:rbenv][:version]}") }
-  ruby_version node[:rbenv][:version]
-  # ここでglobal trueとすると、恐らくこの時点でrbenvの制御下に入り、Berksfileで指定したcookbookがrequireできなくなってしまう。
-  global true
+execute "rbenv.global" do
+  command "echo 2.1.0 > /home/yukkuri/rbenv/version"
 end
